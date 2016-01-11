ea0f9f3f2ec3984d011ed1899fe3c92260dc9be5
kuelye <kuelye@gmail.com>
2016-01-12 01:04:20 +0300

    add RadialBarData

--- library/src/main/java/com/kuelye/radialbarchart/library/RadialBarChartView.java
+++ library/src/main/java/com/kuelye/radialbarchart/library/RadialBarChartView.java
@@ -19,13 +19,19 @@ package com.kuelye.radialbarchart.library;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+import java.util.List;
+
 public class RadialBarChartView extends View {
 
-  @NonNull protected Paint mPaint;
+  @NonNull private Paint mPaint;
+  @Nullable private List<RadialBarData> mData;
 
   public RadialBarChartView(Context context, AttributeSet attrs) {
     super(context, attrs);
@@ -38,7 +44,29 @@ public class RadialBarChartView extends View {
   protected void onDraw(Canvas canvas) {
     super.onDraw(canvas);
 
-    canvas.drawCircle(0, 0, 100, mPaint);
+    int chartTop = getPaddingTop();
+    int chartBottom = getMeasuredHeight() - getPaddingBottom();
+    int chartLeft = getPaddingLeft();
+    int chartRight = getMeasuredWidth() - getPaddingRight();
+    RectF chartRect = new RectF(chartLeft, chartTop, chartRight, chartBottom);
+    
+    canvas.drawArc(chartRect, 10, 200, false, mPaint);
+  }
+
+  public void updateData(List<RadialBarData> data) {
+    mData = data;
+
+    notifyDataUpdated();
+  }
+
+  public void notifyDataUpdated() {
+    invalidate();
+  }
+
+  /* ========================== HIDDEN ============================== */
+
+  private void drawRadialBar() {
+
   }
 
 }
---
+++ library/src/main/java/com/kuelye/radialbarchart/library/RadialBarData.java
@@ -0,0 +1,59 @@
+package com.kuelye.radialbarchart.library;
+
+/*
+ * Copyright 2016 Alexey Leshchuk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class RadialBarData {
+
+  private int mLevel;
+  private float mValue;
+
+  private RadialBarData(Builder builder) {
+    mLevel = builder.mLevel;
+    mValue = builder.mValue;
+  }
+
+  public int getLevel() {
+    return mLevel;
+  }
+
+  public float getValue() {
+    return mValue;
+  }
+
+  /* =========================== INNER ============================== */
+
+  public static class Builder {
+
+    private int mLevel = 0;
+
+    private float mValue = 0;
+
+    public Builder() {
+      // stub
+    }
+
+    public void setValue(float value) {
+      mValue = value;
+    }
+
+    public void setLevel(int level) {
+      mLevel = level;
+    }
+
+  }
+
+}
