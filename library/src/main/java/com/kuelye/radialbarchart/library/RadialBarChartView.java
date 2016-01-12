4d759c20c9088dd9dbba146da386035f48f1487d
kuelye <kuelye@gmail.com>
2016-01-12 13:38:21 +0300

    implement artTo with qubic bezier path

--- library/src/main/java/com/kuelye/radialbarchart/library/RadialBarChartView.java
+++ library/src/main/java/com/kuelye/radialbarchart/library/RadialBarChartView.java
@@ -20,7 +20,6 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.RectF;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -28,10 +27,16 @@ import android.view.View;
 
 import java.util.List;
 
+import static java.lang.Math.PI;
+
 public class RadialBarChartView extends View {
 
   @NonNull private Paint mPaint;
   @Nullable private List<RadialBarData> mData;
+
+  private double mStartAngleInRadians = 0;
+  private double mEndAngleInRadians = PI / 2;
+  private float mRadiusModifier = 1f;
 
   public RadialBarChartView(Context context, AttributeSet attrs) {
     super(context, attrs);
@@ -44,17 +49,70 @@ public class RadialBarChartView extends View {
   protected void onDraw(Canvas canvas) {
     super.onDraw(canvas);
 
-    int chartTop = getPaddingTop();
-    int chartBottom = getMeasuredHeight() - getPaddingBottom();
-    int chartLeft = getPaddingLeft();
-    int chartRight = getMeasuredWidth() - getPaddingRight();
-    RectF chartRect = new RectF(chartLeft, chartTop, chartRight, chartBottom);
-    
-    canvas.drawArc(chartRect, 10, 200, false, mPaint);
+    final int chartViewTop = getPaddingTop();
+    final int chartViewBottom = getMeasuredHeight() - getPaddingBottom();
+    final int chartViewLeft = getPaddingLeft();
+    final int chartViewRight = getMeasuredWidth() - getPaddingRight();
+    final int chartCenterX = (chartViewLeft + chartViewRight) / 2;
+    final int chartCenterY = (chartViewTop + chartViewBottom) / 2;
+    final int chartSize = Math.min(chartViewRight - chartViewLeft, chartViewBottom - chartViewTop);
+    final float r = chartSize * mRadiusModifier / 2f;
+    final int xc = chartCenterX;
+    final int yc = chartCenterY;
+
+    final float x1 = (float) (chartCenterX + r / 2f * Math.sin(mStartAngleInRadians));
+    final float y1 = (float) (chartCenterY - r / 2f * Math.cos(mStartAngleInRadians));
+    final float x4 = (float) (chartCenterX + r / 2f * Math.sin(mEndAngleInRadians));
+    final float y4 = (float) (chartCenterY - r / 2f * Math.cos(mEndAngleInRadians));
+
+    drawMarker(canvas, chartCenterX, chartCenterY);
+    drawMarker(canvas, x1, y1);
+    drawMarker(canvas, x4, y4);
+
+    final double ax = x1 - xc;
+    final double ay = y1 - yc;
+    final double bx = x4 - xc;
+    final double by = y4 - yc;
+    final double q1 = ax * ax + ay * ay;
+    final double q2 = q1 + ax * bx + ay * by;
+    final double k2 = 4d / 3d * (Math.sqrt(2d * q1 * q2) - q2) / (ax * by - ay * bx);
+
+    final float x2 = (float) (xc + ax - k2 * ay);
+    final float y2 = (float) (yc + ay + k2 * ax);
+    final float x3 = (float) (xc + bx + k2 * by);
+    final float y3 = (float) (yc + by - k2 * bx);
+
+    drawMarker(canvas, x2, y2);
+    drawMarker(canvas, x3, y3);
+
+    final Path path = new Path();
+    path.moveTo(x1, y1);
+    path.cubicTo(x2, y2, x3, y3, x4, y4);
+
+    canvas.drawLine(10, 10, 100, 100, mPaint);
+    canvas.drawPath(path, mPaint);
   }
 
   public void updateData(List<RadialBarData> data) {
     mData = data;
+
+    notifyDataUpdated();
+  }
+
+  public void updateStartAngle(int startAngleInDegrees) {
+    mStartAngleInRadians = startAngleInDegrees * PI / 180;
+
+    notifyDataUpdated();
+  }
+
+  public void updateEndAngle(int endAngleInDegrees) {
+    mEndAngleInRadians = endAngleInDegrees * PI / 180;
+
+    notifyDataUpdated();
+  }
+
+  public void updateRadiusModifier(float radiusModifier) {
+    mRadiusModifier = radiusModifier;
 
     notifyDataUpdated();
   }
@@ -67,6 +125,13 @@ public class RadialBarChartView extends View {
 
   private void drawRadialBar() {
 
+  }
+
+  private void drawMarker(@NonNull Canvas canvas, float x, float y) {
+    final Paint markerPaint = new Paint(0);
+    markerPaint.setColor(0xff00ff00);
+
+    canvas.drawCircle(x, y, 10, markerPaint);
   }
 
 }
--- sample/src/main/java/com/kuelye/radialbarchart/sample/ShowcaseActivity.java
+++ sample/src/main/java/com/kuelye/radialbarchart/sample/ShowcaseActivity.java
@@ -19,6 +19,9 @@ package com.kuelye.radialbarchart.sample;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.SeekBar;
+
+import com.kuelye.radialbarchart.library.RadialBarChartView;
 
 public class ShowcaseActivity extends Activity {
 
@@ -26,6 +29,52 @@ public class ShowcaseActivity extends Activity {
   public void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.showcase_activity);
+
+    final RadialBarChartView radialBarChartView
+        = (RadialBarChartView) findViewById(R.id.showcase_activity_radialbarchartview);
+    final SeekBar startAngleSeekBar = (SeekBar) findViewById(R.id.showcase_activity_start_angle_seekbar);
+    startAngleSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListenerStub() {
+
+      @Override
+      public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        radialBarChartView.updateStartAngle(progress);
+      }
+
+    });
+    final SeekBar endAngleSeekBar = (SeekBar) findViewById(R.id.showcase_activity_end_angle_seekbar);
+    endAngleSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListenerStub() {
+
+      @Override
+      public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        radialBarChartView.updateEndAngle(progress);
+      }
+
+    });
+    final SeekBar radiusModifierSeekBar = (SeekBar) findViewById(R.id.showcase_activity_radius_modifier_seekbar);
+    radiusModifierSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListenerStub() {
+
+      @Override
+      public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        radialBarChartView.updateRadiusModifier(progress / 100f);
+      }
+
+    });
+  }
+
+  /* ========================== INNER =============================== */
+
+  private abstract class OnSeekBarChangeListenerStub implements SeekBar.OnSeekBarChangeListener {
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+      // stub
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+      // stub
+    }
+
   }
 
 }
